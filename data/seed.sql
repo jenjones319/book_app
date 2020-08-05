3aba2ff021364e8176b47f26f0dde78d83b1692c
Jennifer B Jones <66266330+jenjones319@users.noreply.github.com>
2020-08-05 13:54:04 -0500

    table working in psql

--- data/seed.sql
+++ data/seed.sql
@@ -1,2 +1,2 @@
-INSERT INTO bookshelf (id, author, title, isbn, image_url, category, description) 
-VALUES('author','title','image_url','category','description');
+INSERT INTO bookshelf (author, title, isbn, image_url, category, descriptions) 
+VALUES('author','title','isbn','image_url','category','descriptions');
