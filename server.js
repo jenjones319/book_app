818cc5ff58b2880cc6ef0c9c2d347f4e1de1468b
Enrique Hilst <enrique.hilst@gmail.com>
2020-08-05 10:17:23 -0500

    added detail SQL

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 'use strict';
+
 // Application Dependencies
 const express = require('express');
 const superagent = require('superagent');
+
 // Application Setup
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 // Application Middleware
 app.use(express.urlencoded({ extended: true }));
+
 // Set the view engine for server-side templating
 app.set('view engine', 'ejs');
 app.use(express.static('./public'));
+
 // API Routes
 // Renders the home page
 app.get('/', renderHomePage);
@@ -17,12 +22,15 @@ app.get('/', renderHomePage);
 // Renders the search form
 app.get('/searches/new', showForm);
 app.get('/new', showForm);
+app.get('/books/:id', getBook);
 
 // Creates a new search to the Google Books API
 app.post('/searches', createSearch);
+
 // Catch-all
 app.get('*', (request, response) => response.status(404).send('This route does not exist'));
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+
 // HELPER FUNCTIONS
 function Book(info) {
   const placeholderImage = 'https://i.imgur.com/J5LVHEL.jpg';
@@ -31,16 +39,37 @@ function Book(info) {
   this.authors = info.authors || 'No authors available';
   this.description = info.description || 'No description availble'
 }
-// Note that .ejs file extension is not required
+
 function renderHomePage(request, response) {
   response.render('pages/index');
 }
+
 function showForm(request, response) {
   response.render('pages/new.ejs');
 }
+
+function getBook(request, response) {
+  const SQL = `
+    SELECT *
+    FROM books 
+    WHERE id = $1
+    `;
+
+  let values = [request.params.id];
+  clientInformation.query(SQL, values)
+    .then(result => {
+      let viewModel = {
+        task: result.rows[0],
+      };
+      response.render('pages/detail', viewModel);
+    })
+    .catch(error => errorHandler(error, response));
+}
+
 function errorHandler(error, request, response, next) {
   response.status(500).json({ error: true, message: error.message });
 }
+
 // No API key required
 // Console.log request.body and request.body.search
 function createSearch(request, response) {
